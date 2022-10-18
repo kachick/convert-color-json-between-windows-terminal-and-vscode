788b2e29442173eef6fe40cb115c44453c38d21c
Kenichi Kamiya <kachick1@gmail.com>
2022-10-19 04:48:25 +0900

    dprint fmt

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ type VSCodeColorSchema = {
 };
 
 const convertWindowsTerminalSchemaToVSCodeSchema = (
-  windowsTerminalSchema: Partial<WindowsTerminalColorSchema>
+  windowsTerminalSchema: Partial<WindowsTerminalColorSchema>,
 ) => {
   const vscodeSchema: Partial<VSCodeColorSchema> = {};
   Object.entries(windowsTerminalSchema).forEach(([key, value]) => {
@@ -84,35 +84,36 @@ function App() {
   };
 
   return (
-    <div className="App pure-g site-all">
-      <p className="pure-u-1-5"></p>
-      <div className="pure-u-3-5">
-        <header className="App-header">
+    <div className='App pure-g site-all'>
+      <p className='pure-u-1-5'></p>
+      <div className='pure-u-3-5'>
+        <header className='App-header'>
           <img
-            src="https://upload.wikimedia.org/wikipedia/commons/5/51/Windows_Terminal_logo.svg"
-            className="pure-img"
-            alt="logo"
+            src='https://upload.wikimedia.org/wikipedia/commons/5/51/Windows_Terminal_logo.svg'
+            className='pure-img'
+            alt='logo'
             width={headerLogoSize}
             height={headerLogoSize}
           />
           <img
-            src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg"
-            className="eader-logo pure-img"
-            alt="logo"
+            src='https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg'
+            className='eader-logo pure-img'
+            alt='logo'
             width={headerLogoSize}
             height={headerLogoSize}
           />
         </header>
-        <main className="site-content">
-          <form className="pure-form">
+        <main className='site-content'>
+          <form className='pure-form'>
             <textarea
               rows={25}
               cols={60}
-              placeholder="Take from Windows Terminal. Paste your whole settings.json or the color schema part."
+              placeholder='Take from Windows Terminal. Paste your whole settings.json or the color schema part.'
               onChange={(e) => setInputtedText(e.currentTarget.value)}
               required={true}
-              className="pure-input pure-u-1-1 original-json"
-            ></textarea>
+              className='pure-input pure-u-1-1 original-json'
+            >
+            </textarea>
             <button
               disabled={!inputtedText}
               onClick={(e) => {
@@ -121,47 +122,45 @@ function App() {
                 setIsVisibleCopied(false);
                 convertWindowsTerminalToVSCode();
               }}
-              className="pure-u-1-3 pure-button convert-button pure-button-primary button-next"
+              className='pure-u-1-3 pure-button convert-button pure-button-primary button-next'
             >
               Convert!
             </button>
           </form>
-          <p className="convert-errors" hidden={errors.length === 0}>
+          <p className='convert-errors' hidden={errors.length === 0}>
             You are converting invalid JSON
-            {errors.map((e, idx) => (
-              <li key={idx}>{e.message}</li>
-            ))}
+            {errors.map((e, idx) => <li key={idx}>{e.message}</li>)}
           </p>
           <div hidden={!isVisibleConvertedJSON}>
-            <pre className="converted-area">{outputText}</pre>
+            <pre className='converted-area'>{outputText}</pre>
             <button
               onClick={(e) => {
                 e.preventDefault();
                 navigator.clipboard.writeText(outputText).then(() => setIsVisibleCopied(true));
               }}
-              className="pure-u-1-3 pure-button pure-button-primary button-next"
+              className='pure-u-1-3 pure-button pure-button-primary button-next'
             >
               Copy!
             </button>
-            <p className="notice-copied" hidden={!isVisibleCopied}>
+            <p className='notice-copied' hidden={!isVisibleCopied}>
               Copied to your clipboard!
             </p>
           </div>
         </main>
-        <footer className="pure-menu pure-menu-horizontal">
+        <footer className='pure-menu pure-menu-horizontal'>
           <p>
             <a
-              className="pure-menu-heading pure-menu-link App-link"
-              href="https://github.com/kachick/convert-color-json-between-windows-terminal-and-vscode"
-              target="_blank"
-              rel="noopener noreferrer"
+              className='pure-menu-heading pure-menu-link App-link'
+              href='https://github.com/kachick/convert-color-json-between-windows-terminal-and-vscode'
+              target='_blank'
+              rel='noopener noreferrer'
             >
               Repository
             </a>
           </p>
         </footer>
       </div>
-      <p className="pure-u-1-5"></p>
+      <p className='pure-u-1-5'></p>
     </div>
   );
 }
