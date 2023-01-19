d4d938637d1563148f96322ebefe2d95ef2b2734
Kenichi Kamiya <kachick1@gmail.com>
2023-01-19 14:08:21 +0900

    WIP

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/8eabb500ebf5dcb9a40024c1150ef348cefa8320.tar.gz") { } }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.deno
+    pkgs.unbuffer # To keep color in tee of local development
+    pkgs.direnv
+    pkgs.dprint
+    pkgs.nil
+    pkgs.nixpkgs-fmt
+  ];
+}
