9109e61ab0ceab72adbf48bcbc4cbcc6fd598e32
Kenichi Kamiya <kachick1@gmail.com>
2023-07-16 00:54:21 +0900

    Fix the specifiers more!

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'npm:vite@^4.4.4';
-import viteReact from 'npm:@vitejs/plugin-react@^4.0.3';
+import { defineConfig } from 'npm:vite@4.4.4';
+import viteReact from 'npm:@vitejs/plugin-react@4.0.3';
 
 // https://vitejs.dev/config/
 export default defineConfig({
