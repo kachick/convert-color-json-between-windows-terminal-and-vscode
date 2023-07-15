581514dff98dfb4a25b77d0aeb9c73dd9a83ac5e
Kenichi Kamiya <kachick1@gmail.com>
2023-07-16 00:47:56 +0900

    Weird, but this is the current type defnition...

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'npm:vite@^4.4.4';
-import react from 'npm:@vitejs/plugin-react@^4.0.3';
+import viteReact from 'npm:@vitejs/plugin-react@^4.0.3';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [viteReact.default()],
 });
