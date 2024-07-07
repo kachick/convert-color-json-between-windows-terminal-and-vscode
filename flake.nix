eae2d45dbe48de0b1b9e8d08c0126a224557cb3b
Kenichi Kamiya <kachick1@gmail.com>
2024-07-07 19:14:32 +0900

    Replace nixpkgs-fmt with nixfmt-rfc-style

--- flake.nix
+++ flake.nix
@@ -9,23 +9,32 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-        devShells.default = with pkgs;
+        formatter = pkgs.nixfmt-rfc-style;
+        devShells.default =
+          with pkgs;
           mkShell {
             buildInputs = [
               # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
               bashInteractive
 
               nil
-              nixpkgs-fmt
+              nixfmt-rfc-style
               deno
               typos
             ];
           };
-      });
+      }
+    );
 }
